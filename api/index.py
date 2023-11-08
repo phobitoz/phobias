214436b9cf38d696fa823d72a75ef52035c1f8e4
Phoboz <122142766+phobitoz@users.noreply.github.com>
2023-11-08 17:37:49 -0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,31 @@
+
 from http.server import BaseHTTPRequestHandler
 
-class handler(BaseHTTPRequestHandler):
+class CustomHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+
+        # HTML que muestra una imagen centrada y un encabezado h1
+        response_content = """
+        <html>
+        <head>
+            <title>Under Construction</title>
+        </head>
+        <body style="text-align: center;">
+            <h1 style="color: #333;">This site is under construction</h1>
+            <img src="404.png" alt="404" style="display: block; margin: 0 auto;">
+        </body>
+        </html>
+        """
+
+        self.wfile.write(response_content.encode('utf-8'))
         return
+
+if __name__ == '__main__':
+    from http.server import HTTPServer
+    server = HTTPServer(('localhost', 8080), CustomHandler)
+    print('Servidor web en ejecución...')
+    server.serve_forever()
